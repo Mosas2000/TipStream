472463328257ec495d35b5a80610bd4b2b1f5af5
0xMosas <tosinolosasa@gmail.com>
2026-03-14 10:16:53 +0100

    feat: add Access-Control-Max-Age header to reduce preflight requests

--- chainhook/server.js
+++ chainhook/server.js
@@ -166,6 +166,7 @@ const server = http.createServer(async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400");
 
   // Handle CORS preflight requests
   if (req.method === "OPTIONS") {
