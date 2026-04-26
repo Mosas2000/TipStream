bae81cb1a14c5a3a0141bcc060b55134ee030bba
0xMosas <tosinolosasa@gmail.com>
2026-04-26 16:08:38 +0100

    Standardize tip amount in platform stats tracking test

--- tests/tipstream.test.ts
+++ tests/tipstream.test.ts
@@ -193,7 +193,7 @@ describe("TipStream Contract Tests", () => {
             "send-tip",
             [
                 Cl.principal(wallet2),
-                Cl.uint(1000000),
+                Cl.uint(MOCK_TIP_AMOUNT),
                 Cl.stringUtf8("Tip 1")
             ],
             wallet1
